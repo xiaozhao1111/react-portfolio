fb384df437a40cf07dc5d12356e733074f7b18e0
xiaozhao1111 <zhaoxiao20071106@gmail.com>
2023-03-14 08:14:40 +0000

    hero css updated

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,19 @@
 import React from "react";
 import photo from '../assets/images/photo.jpeg';
+import './Hero.css'
 
 
 function Hero() {
     return( 
-        <>
+        
+        <div className="hero">
             <div class="name-photo">
             <img src={photo} alt="photo" />
             <h1>Xiao Zhao</h1>
+            </div>
+            <h2>Stay Hungry! Stay Foolish!</h2>
         </div>
-        <h2>Stay Hungry! Stay Foolish!</h2>
-        </>
+            
     )
 }
 
