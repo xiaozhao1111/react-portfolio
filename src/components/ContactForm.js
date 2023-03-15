2dd32bd5ffc74c9246fcf17a9e49e2f27381d707
xiaozhao1111 <zhaoxiao20071106@gmail.com>
2023-03-15 00:02:24 +0000

    build app

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 function ContactForm() {
-    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+    }
     return(
         <div className="mt-5 text-white">
         <h1 className="text-center">Contact Me</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type='text' placeholder="Enter Your Name" />
             <input type='text' placeholder="Enter Your Email Address" />
             <textarea type='text' placeholder="Enter Your Message..." col='30' rows='10'></textarea> 
-            <button onClick={alertUser}>Submit</button>
+            <button>Submit</button>
         </form>
         </div>
     )
